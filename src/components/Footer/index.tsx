4350dbfe9b0d6b65c96b1b7ab49f8e52c0480969
Sergio Garcia <sergio_sgz@hotmail.com>
2022-10-03 11:52:36 +0200

    added_header_and_footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+export const Footer = (): JSX.Element => {
+  return (
+    <footer className={styles.footer}>
+      <p>Albumster by Sergio Garcia Zurera</p>
+    </footer>
+  )
+}
