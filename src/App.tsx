46499c2417cf143d7faf9314a42213d3db313977
Sergio Garcia <sergio_sgz@hotmail.com>
2022-10-03 11:48:27 +0200

    added_routes_app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(): JSX.Element {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Layout>
+        <Routes>
+          <Route
+            path="albumList"
+            element={
+              <React.Suspense>
+                <AlbumList />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="/albumList/addAlbum"
+            element={
+              <React.Suspense>
+                <AddAlbum />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="/albumList/albumInfo"
+            element={
+              <React.Suspense>
+                <AlbumInfo />
+              </React.Suspense>
+            }
+          />
+        </Routes>
+      </Layout>
     </div>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css'
+import logo_header from '../../assets/logo_albumster.svg'
+import { Link } from 'react-router-dom'
+export const Header = (): JSX.Element => {
+  return (
+    <header className={styles.header}>
+      <Link to="albumList">
+        <img src={logo_header} className={styles.logo} />
+      </Link>
+    </header>
+  )
+}
